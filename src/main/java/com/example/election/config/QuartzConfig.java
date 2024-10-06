1dbe1566d0815e6483bd6c2c27576c82e7ed922e
Pasindu Muthumala <lgpmuthumala@gmail.com>
2024-10-06 23:21:15 +0530

    Temporary commit: added scheduler jobs and storing them in database with quartz to start election jobs at given start date: there is an issue for use multiple databases for quartz job stores. currently those are stored in the election_db.(ii)

--- src/main/java/com/example/election/config/QuartzConfig.java
+++ src/main/java/com/example/election/config/QuartzConfig.java
@@ -1,6 +1,9 @@
 package com.example.election.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.example.election.utils.quartz.job_factory.AutowiringSpringBeanJobFactory;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -12,31 +15,49 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@EnableAutoConfiguration
 public class QuartzConfig {
 
-//    @Bean
-//    @QuartzDataSource
-//    public DataSource quartzDataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://localhost:5432/quartz_job_db");
-//        dataSource.setUsername("postgres");
-//        dataSource.setPassword("pgql");
-//        return dataSource;
-//    }
+    @Autowired
+    private AutowiringSpringBeanJobFactory jobFactory;
+
+    @Bean
+    public AutowiringSpringBeanJobFactory jobFactory() {
+        return new AutowiringSpringBeanJobFactory();
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(DataSource quartzDataSource) {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setDataSource(quartzDataSource);
+        factory.setJobFactory(jobFactory);
+        return factory;
+    }
+
+    @Bean
+    public Scheduler scheduler(SchedulerFactoryBean schedulerFactoryBean) throws SchedulerException {
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    @Bean
+    @QuartzDataSource
+    public DataSource quartzDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/quartz_job_db");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("pgql");
+        return dataSource;
+    }
 
     @Bean
     public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer() {
-        return new SchedulerFactoryBeanCustomizer() {
-            @Override
-            public void customize(SchedulerFactoryBean bean) {
-                bean.setQuartzProperties(createQuartzProperties());
-            }
-        };
+        return bean -> bean.setQuartzProperties(createQuartzProperties());
     }
 
-    private Properties createQuartzProperties() {
+    @Bean
+    public Properties createQuartzProperties() {
         Properties props = new Properties();
         props.put("spring.quartz.properties.org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate");
         return props;
