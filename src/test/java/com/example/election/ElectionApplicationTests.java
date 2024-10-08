42cb4807f074f2167a21396e9c80eb0825d42e6a
Pasindu Muthumala <lgpmuthumala@gmail.com>
2024-10-08 18:45:19 +0530

    Added .env properties to testing.

--- src/test/java/com/example/election/ElectionApplicationTests.java
+++ src/test/java/com/example/election/ElectionApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.election;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,16 @@ class ElectionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@BeforeAll
+	static void setup() {
+		loadEnv();
+	}
+
+	public static void loadEnv() {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 	}
 
 }
